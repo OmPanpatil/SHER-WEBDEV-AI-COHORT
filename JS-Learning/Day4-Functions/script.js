6a52be6d61be51e569e94fda132fb98da52f7769
Om Panpatil <ompanpatil513@gmail.com>
2025-02-20 21:51:31 +0530

    JS

---
+++ JS-Learning/Day4-Functions/script.js
@@ -0,0 +1,18 @@
+// What is function : Kuch code jo aapka
+// It is a block of code which can be resued and reduse 
+
+// function abcd() {
+    
+// }
+
+function ghar(){
+    console.log("ghar aao");
+    console.log("Gate kholo");
+    console.log("calisthenics karro");
+    console.log("Code karro");
+    console.log("Have dinner");
+    
+    
+}
+
+ghar();
