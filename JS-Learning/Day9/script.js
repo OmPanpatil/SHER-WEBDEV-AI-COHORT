bcc4aed47ea0ede45e51c241bc7a3a80acdc7315
Om Panpatil <ompanpatil513@gmail.com>
2025-02-28 21:41:48 +0530

    JS

---
+++ JS-Learning/Day9/script.js
@@ -0,0 +1,19 @@
+// Arrays
+
+console.log("Question 1 : Duplicate");
+
+var arr = [1, 1, 2, 3, 2, 3, 5, 6, 5, 6];
+var ans = [...new Set(arr)];
+console.log(ans);
+
+console.log("Question 2 : Sorting, uniqueness and indexing");
+
+let arr1 = [5,4,6, 455, 655, 8];
+
+// let ans1 = arr1.sort()
+
+// var ans2 = []
+
+
+
+
