926f3653ba2a11f1cb2f48237d7e7c81264dc8fa
ompanpatil <ompanpatil513@gmail.com>
2025-03-17 21:57:28 +0530

    Patterns

---
+++ DSA/Day7/script.js
@@ -0,0 +1,10 @@
+// for(let i=1;i<=5;i++){
+//     console.log("*");
+// }
+
+Process.stdout.write("* ");
+Process.stdout.write("* ");
+Process.stdout.write("* ");
+Process.stdout.write("* ");
+Process.stdout.write("* ");
+
