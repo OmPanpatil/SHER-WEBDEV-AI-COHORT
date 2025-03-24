3cadd50c79309ea4c491ebe829c9a323374b6df3
Om Panpatil <ompanpatil513@gmail.com>
2025-03-24 22:07:29 +0530

    DOM

---
+++ Js-Dom/Day-1/script.js
@@ -0,0 +1,13 @@
+// var h = document.querySelector('h1');
+// console.log(h);
+
+var h1 = document.querySelector('h1');
+h1.addEventListener('click',function(){
+
+    // var h = document.querySelector('h1');
+    // console.log(h);
+    h1.innerHTML='Nice';
+    h1.style.color = 'red';
+});
+
+
