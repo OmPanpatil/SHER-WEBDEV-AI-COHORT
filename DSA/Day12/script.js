6e949649f262ffc5dfae3841af363ac16eb99bb0
Om Panpatil <ompanpatil513@gmail.com>
2025-03-28 21:45:17 +0530

    DSA

---
+++ DSA/Day12/script.js
@@ -0,0 +1,37 @@
+// let arr = [1,4,5,3,2,9,10];
+
+// for(phase)  outer
+
+// for(let i=0; i<arr.length-1; i++){
+//     for(let j=0; j<arr.length-i-1; j++){
+//         if(arr[j]>arr[j+1]){
+//             let temp = arr[j];
+//             arr[j]= arr[j+1];
+//             arr[j+1] = temp;
+//         }
+//     }
+// }
+// console.log(arr);
+
+
+// let arr = [1,4,5,3,2,9,10];
+
+// for(let i=0; i<arr.length-1; i++){
+//     let small = i;
+//     for(let j=i+1; j<arr.length; j++){
+//         if(arr[small]> arr[j]){
+//             small = j;
+//         }
+//     }
+
+//     if(i != small){
+//         let temp = arr[i];
+//         arr[i] = arr[small];
+//         arr[small]= temp;
+//     }
+// }
+
+// console.log(arr);
+
+
+
