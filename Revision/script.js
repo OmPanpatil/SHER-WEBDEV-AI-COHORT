4ff5f127bc44f171f49df11d7540208bada3ad59
Om Panpatil <ompanpatil513@gmail.com>
2025-03-30 20:25:13 +0530

    JS

--- Revision/script.js
+++ Revision/script.js
@@ -1,3 +1,16 @@
 console.log("Hloo");
 console.warn('This is js, no turnning back!');
 console.error('No error');
+
+// Variable :
+let a = 10;   //Declaration and initialization
+// let, var and const for constants
+let b = 20;
+console.log(a , b);
+
+// confirm('Are u 18+ ');
+
+// prompt('Enter ur name : ');
+
+// Note : alert for messages, prompt for input or answer, confirm : to confirm yes/no 
+
