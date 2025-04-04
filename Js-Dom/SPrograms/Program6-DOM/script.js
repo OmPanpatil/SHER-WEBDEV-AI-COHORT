7041d863c1c7b411219406f61e23c1d2ccabf6dc
Om Panpatil <ompanpatil513@gmail.com>
2025-04-04 17:09:09 +0530

    DOM

--- Js-Dom/SPrograms/Program6-DOM/script.js
+++ Js-Dom/SPrograms/Program6-DOM/script.js
@@ -11,4 +11,27 @@ btn.addEventListener('click', function(){
     console.log(images);
     var bodies = document.querySelector('body');
     bodies.appendChild(images);
+
+    images.setAttribute('class', 'imege');
+    var x = Math.random()*80;
+    var y = Math.random()*50;
+    var z = Math.random()*50;
+    var rot = Math.random()*360;
+    images.style.left = x + '%';
+    images.style.top = y + '%';
+    images.style.position = z + '%';
+    images.style.rotate = rot + 'deg';
 })
+
+
+// let arr = ['CSK', 'MI', 'RCB', 'KKR', 'SRH', 'DC', 'PBKS', 'RR', 'LSG', 'GT'];
+// // console.log(arr);
+// let btn = document.querySelector('button');
+// let heading2 = document.querySelector('h2');
+
+// btn.addEventListener('click', function(){
+//     let num = Math.floor(Math.random()*arr.length);
+//     let winner = arr[num];
+//     console.log(winner);
+//     heading2.innerHTML = winner;
+// })
