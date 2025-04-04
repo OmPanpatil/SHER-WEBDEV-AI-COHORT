46462505f761d01cf0cbcbd41cb70dc530278a49
Om Panpatil <ompanpatil513@gmail.com>
2025-04-04 22:10:49 +0530

    DOM

---
+++ Js-Dom/Day-5/script.js
@@ -0,0 +1,35 @@
+// var arr = [10,20,30,40,50];
+// console.log(arr);
+// arr.forEach(function(){
+//     console.log("Hello world!");
+// })
+
+// var arr = ['sarthak', 'salauddin'];
+// arr.forEach(function(name, idx){
+//     console.log(name , idx);
+// });
+
+// first position is for element and second is var for index.
+
+// var arr = [
+//     {
+//         username: 'Om',
+//         age : 30
+//     }, 
+//     {
+//         username: 'sarthak',
+//         age : 30
+//     }, 
+//     {
+//         username: 'salauddin',
+//         age : 30
+//     }, 
+//     {
+//         username: 'Om',
+//         age : 30
+//     }
+// ];    //array of objects
+
+// console.log(arr[1].username, arr[1].age);
+
+
