a9542b2942f9b9e52c8875acabfa61cfdea314e8
Om Panpatil <ompanpatil513@gmail.com>
2025-04-06 20:26:16 +0530

    Revise

--- Revision/script.js
+++ Revision/script.js
@@ -78,5 +78,133 @@ console.log(r + s);
 // a) primitive : number(10,10.5,10.555), string, boolean, undefined, null, symbol, bigint
 // b) referencs : array, objects, functions.
 
+// var aa = 'om';
+// var bb = 'Panpatil';
+// console.log(aa + bb);  //Concatenation
+// console.log(aa-bb);  //NaN : not a number 
+
+// var aa = Number(prompt('Enter first number : '));
+// var bb = Number(prompt('Enter second number : '));
+// console.log(aa + bb);
+
+// var aaa = null;
+// console.log(aaa);
+// var aa;
+// console.log(aa);
+
+// symbol : unique and immutable data type.
+// var sym = Symbol('om);
+// console.log(sym);
+
+// var aa = 100000000000000000000000000000000000000000000000000000000000n;
+// console.log(aa);
+// Bigint is different from number. It is used to store large numbers.
+
+// Undefined : Var is declared but not initialized.
+// Null : Var is declared and initialized but no value is assigned to it.
+
+// console.log(aa);
+// var aa  = 10; //Hositing : var is hoisted to the top of the functions or global scope. Firstly, var is declared and then initialized.  output will be undefined.
+
+// CONDITIONALS : if-else, else-if, switch-case, break, continue.
+
+// let age = Number(prompt('Enter ur age : '));
+
+// if(age>18 && age<60){
+//     console.log('U are a voter.');
+// } else if(age>60){
+//     console.log('U are a senior citizen.');
+// } else{
+//     console.log('U are not a voter.');
+// }
 
 
+// Binary Operators : +,-,*,/,%,**(exponential), ++(increment), --(decrement), =, +=, -=, *=, /=, %=, **=
+// Logical Operators : &&, ||, !
+// Comparison Operators : ==, ===, !=, !==, >, <, >=, <=
+// Bitwise Operators : &, |, ^, ~, <<, >>
+// Ternary Operators : condition ? true : false
+// Assignment Operators : =, +=, -=, *=, /=, %=, **=
+// Identity Operators : ===, !==
+// Instance Operators : instanceof, in, typeof
+// Spread Operators : ... (used to spread the elements of an array or object.)
+// Destructuring Operators : {key:value} (used to extract the values of an object and assign them to variables.)
+// Optional Chaining Operators : ? (used to check if the value is null or undefined.)
+// Nullish Coalescing Operators : ?? (used to check if the value is null or undefined.)
+// Nullish Coalescing Assignment Operators : ??= (used to assign a value to a variable if the variable is null or undefined.)
+
+// Exponential Operators : ** (used to calculate the power of a number.)
+// console.log(2**3); // 8
+
+
+
+// true = 1; false = 0;
+
+// && => AND : Both the conditions must be true compusory. Then it will be a true value.
+
+// 0 0 == 0
+// 0 1 == 0
+// 1 0 == 0
+// 1 1 == 1
+
+// || => OR : If the one of the conditions is true, then it will be true always.
+
+// 0 0 == 0;
+// 0 1 == 1;
+// 1 0 == 1;
+// 1 1 == 1;
+
+
+// == : It compares the values only. It does not check the data types.  Example: 10 == '10' => true.
+// === : It compares the values as well as the data types. It is strictly equal to.  Example: 10 === '10' => false.
+// (!=) : It is not equal to. It compares the values only. It does not check the data types. Example: 10 != '10' => false.
+
+// let unit  = Number(prompt('Enter the unit of electricity : '));
+// let amount = 0;
+// let punit=0;
+
+// if(unit>=100){
+//     punit = 10;
+// } else if(unit>50){
+//     punit = 8;
+// } else{
+//     punit = 5;
+// }
+
+// let amount = unit * punit;
+
+// console.log('The amount is : ' + amount + ' Rs.');
+
+
+// Strings methods : 
+
+// 1. slice : Used to modify array elements. It can add or remove the elements from the array. It takes 3 parameters : (1) index, (2) no of elements to be removed, (3) elements to be added.
+
+// let str = 'Hello world!';
+// let ans = str.slice(0,3);
+// console.log(ans);
+
+// 2. Template litterals : It is used to create strings with operations or expressions.
+
+// let aa = 'om!';
+
+// console.log(`Hello, ${aa} world is waiting for u.`);
+
+// 3. Split : It is used to split the string into an array of strings. It takes 1 parameter : Seperator.
+// let str = ' Hello ji kaise ho ? ';
+// let ans = str.split('i');
+// console.log(ans);
+
+// 4. replace : It is used to replace the string with another string. It takes 2 parameters : (1)old string, (2) new string.
+
+// let str = 'Hello world!';
+// let ans = str.replace('world','om');
+// console.log(ans);
+
+// 5. Includes : It is used to check if the string contains the specified string. It returns true or false.
+let str = 'Hello world!';
+let ans = str.includes('world');
+console.log(ans);
+
+// break : loop ko rokta hai.
+// continue : loop ko waha uss element se skip karta hai.
