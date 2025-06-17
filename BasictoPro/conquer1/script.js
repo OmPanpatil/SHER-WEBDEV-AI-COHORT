845682f8984fa746d782da7255addf9432103302
Om Panpatil <ompanpatil513@gmail.com>
2025-06-17 17:01:49 +0530

    Crusher

---
+++ BasictoPro/conquer1/script.js
@@ -0,0 +1,37 @@
+console.log("Conquer the world!");
+// console.warn("This is a warning message.");
+// console.error("This is a error message.");
+// console.info("This is an onfo message.");
+
+// var a = CSSMathValue;  variable variablename = value;
+// declaration variablename (var a)
+// initialization variablename = value (var a =10;)
+
+var a = 10;
+var b = 20;
+console.log(a+b);
+console.log(a-b);
+console.log(a*b);
+console.log(a/b);
+console.log(a%b);
+console.log(a**b); //Exponentiation operator
+
+console.log(`${a} + ${b} = ${a+b}`, "the result is " + `${a+b}`);
+
+confirm("Do u want to proceed?");
+
+alert("This is an alert message!");
+
+let c = "Om";
+prompt("Enter ur name", c);
+
+let d = 50;
+prompt("Enter a number : ", d);
+
+var ans = prompt("Enter a numberssss : ", b);
+console.log(`The number u entered is ${ans}`);
+
+
+
+
+
